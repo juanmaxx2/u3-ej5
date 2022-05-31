5f24b2346162d47e28a5758d206bb80ca9f25f55
Juan Martin <103914139+juanmaxx2@users.noreply.github.com>
2022-05-31 16:31:35 -0300

    Add files via upload

---
+++ ejercicio 5/interface.py
@@ -0,0 +1,8 @@
+from interface import Interface
+class Interface(Interface):
+    def InstertarElemtno(elemto,posicion):
+        pass
+    def AgregarElemento(elemento):
+        pass
+    def MostrarElemento(elemento):
+        pass
